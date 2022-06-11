65b33795ec8c8f6dfc541635a822de88755e3642
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-06-11 19:17:12 -0300

    mongoose added

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import mongoose from 'mongoose';
 
 type Data = {
   name: string
@@ -11,3 +12,23 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+const postSchema = new mongoose.Schema({
+  roomCode: String,
+  nickName: String,
+  descriptions: {
+    url: String,
+    who: String,
+    what : String,
+    when : String,
+    where: String,
+    why: String,
+    coments: String
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+  }
+})
+
+module.exports = mongoose.model("post", postSchema);
