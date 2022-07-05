dd95a2e3d1e1840e65fb3c0fb839c9d47b2988d9
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-07-05 16:24:48 -0300

    added signin with facebook

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
@@ -6,7 +7,11 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        })
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID!,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+        })  
     ],
     session: {
         maxAge: 30 * 24 * 60 * 60,  // 30 days
